d3d477c5ffcec7129506fd673603158145075d1a
Adarsh <jha01adarsh@gmail.com>
2023-03-11 18:02:23 +0530

    Serving response to browser

--- basichttpserver/index.js
+++ basichttpserver/index.js
@@ -1,13 +1,17 @@
 const http = require("http");
 const port = 8000;
 
-const server = http.createServer();
+function requestHandler(req, res) {
+  console.log(req.url);
+  res.end("Hello Node");
+}
 
+const server = http.createServer(requestHandler);
 
 server.listen(port, (err) => {
   if (err) {
     console.log("error in port :", err);
     return;
   }
-  console.log("server is running on", port);
+  console.log("server is running on port", port);
 });
