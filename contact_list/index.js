bbc55cc49255d710f63ae50cc6d963a1ac53c7e5
Adarsh <jha01adarsh@gmail.com>
2023-03-12 02:32:16 +0530

    Returing response from server

--- contact_list/index.js
+++ contact_list/index.js
@@ -3,6 +3,14 @@ const port = 8000;
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.end("<h1>Hello from Express js</h1>");
+});
+
+app.get("/contactus",(req,res)=>{
+    res.end("<h1>Thanks for contacting us !</h1>")
+})
+
 app.listen(port, (err) => {
   if (err) {
     console.log("Error in port", err);
